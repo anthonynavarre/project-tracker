fcc4269b4e1f63b210a1caa8631fc6d6574d828b
pedrosmmoreira <carameldot@gmail.com>
2013-05-23 14:26:54 +0100

    Add spec to test project owner setter method

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
-describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Project, "#owner=" do
+  it 'assigns the owner of the project to user that creates the project' do
+    user = create(:user)
+    project = create(:project)
+    project.owner = user
+
+    expect(project.owner.email).to eq user.email
+  end
 end
